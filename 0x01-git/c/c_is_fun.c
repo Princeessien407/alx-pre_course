f806d75e0d300b292f789d2730a63c25252a3ba4
prince <princeessien407>
2022-10-12 09:47:36 -0700

    first commit

---
+++ 0x01-git/c/c_is_fun.c
@@ -0,0 +1,13 @@
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+	modified:   ../bash/alx
+	modified:   ../bash/school
+	modified:   ../c_is_fun.c
+	modified:   ../index.js
+	modified:   ../main.js
+
+no changes added to commit (use "git add" and/or "git commit -a")
---
+++ 0x01-git/c_is_fun.c
@@ -0,0 +1,6 @@
+On branch master
+Your branch is up to date with 'origin/master'.
+
+nothing to commit, working tree clean
+[master 5a1cfe7] Starting to code today, so cool
+ 3 files changed, 23 insertions(+)
